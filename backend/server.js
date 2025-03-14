11286d96ad8334e736204e3829570d7e243350a1
Ark <arktheprogrammer@gmail.com>
2025-03-13 22:55:35 -0600

    route stubs

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,53 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+app.post('/auth/login', (req, res) => {
+  const { username, password } = req.body;
+  // TODO: Add real auth later
+  res.json({ status: 'ok' });
+});
+
+// Get mics within date range
+app.get('/mics', (req, res) => {
+  const { start, end } = req.query;
+  // TODO: Add real database query later
+  res.json({
+    mics: [],
+    start,
+    end
+  });
+});
+
+
+// Create a new mic
+app.post('/mics', (req, res) => {
+  const micData = req.body;
+  // TODO: Add database insert
+  res.json({ status: 'ok', mic: micData });
+});
+
+// Read a single mic
+app.get('/mics/:id', (req, res) => {
+  const { id } = req.params;
+  // TODO: Add database query
+  res.json({ status: 'ok', mic: { id } });
+});
+
+// Update a mic
+app.put('/mics/:id', (req, res) => {
+  const { id } = req.params;
+  const micData = req.body;
+  // TODO: Add database update
+  res.json({ status: 'ok', mic: { id, ...micData } });
+});
+
+// Delete a mic
+app.delete('/mics/:id', (req, res) => {
+  const { id } = req.params;
+  // TODO: Add database delete
+  res.json({ status: 'ok', id });
+});
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
