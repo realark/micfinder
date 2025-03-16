03b92bad521df8c94d30d1dd641cc32156e70fb1
Ark <arktheprogrammer@gmail.com>
2025-03-15 21:22:48 -0600

    basic schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,34 @@
+-- Currently we only have two types of queries:
+-- - give me all mics from X date to Y date
+-- - CRUD for mic with id=1234
+-- So, most data is going to be stored de-normalized in a json blob.
+-- Should we desire a normalized db in the future it should be trivial to migrate (because the number of mics is going to be <10 and unlikely to grow larger).
 
-CREATE TABLE mics (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  date TIMESTAMP NOT NULL,
-  venue TEXT,
-  description TEXT
+CREATE EXTENSION if NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- RRULE conforming to RFC 5545: https://datatracker.ietf.org/doc/html/rfc5545
+-- https://freetools.textmagic.com/rrule-generator
+-- null for a one-off event
+CREATE DOMAIN rrule AS VARCHAR(100) CHECK (
+  VALUE IS NULL OR
+  -- TODO: I didn't actually read the spec so this regex is probably wrong
+  VALUE ~ '^FREQ=(DAILY|WEEKLY|MONTHLY|YEARLY)(;\s*(INTERVAL=\d+|BYDAY=[A-Z,]+|BYMONTH=\d+|BYMONTHDAY=[0-9,]+|BYSETPOS=\d+|UNTIL=\d+))*$'
 );
+
+CREATE TABLE app_user (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  email citext NOT NULL unique,
+  full_name TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
+
+CREATE TABLE mic (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  edit_version BIGINT NOT NULL DEFAULT 0,
+  start_date DATE NOT NULL,
+  recurrence RRULE,
+  data JSONB NOT NULL,
+  last_edited_by UUID NOT NULL REFERENCES app_user(id)
+);
+CREATE INDEX mic_start_date_idx ON mic(start_date);
