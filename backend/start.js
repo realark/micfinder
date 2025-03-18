520533e6f313db1141f2b51d676dcc58bb08d479
Ark <ark@recursive.games>
2025-03-17 19:04:31 -0600

    deployment fuckery 2/??

--- backend/start.js
+++ backend/start.js
@@ -1,5 +1,6 @@
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const app = require('./server');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 if (require.main === module) {
   app.listen(port, () => {
