7a0968131c315d95df8ff46a868cab9b01643b28
Ark <ark@recursive.games>
2025-03-27 21:05:18 -0600

    google analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import MicFinder from './components/MicFinder'
 
 function App() {
+  useEffect(() => {
+    const analyticsEnabled = import.meta.env.VITE_ENABLE_ANALYTICS;
+    if (analyticsEnabled === 'true') {
+      const script = document.createElement('script');
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=G-8X2R296EGP';
+      script.async = true;
+      document.head.appendChild(script);
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'G-8X2R296EGP');
+    }
+  }, []);
+
   return (
     <div>
       <MicFinder />
